1204746da05259140d135e7a5061f81451182f5b
snd-primary <snd.webdev@gmail.com>
2026-01-24 17:54:59 +0900

    fix:css path

--- src/shared/ui/layouts/GlobalLayout.tsx
+++ src/shared/ui/layouts/GlobalLayout.tsx
@@ -1,5 +1,5 @@
 import type React from "react";
-import { css } from "../../../styled-system/css";
+import { css } from "../../../../styled-system/css";
 
 type Props = {
 	children: React.ReactNode;
